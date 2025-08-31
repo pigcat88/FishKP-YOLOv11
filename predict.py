1aa651572f7d726d885e87303549a762c2f6fd77
pigcat <52456159+pigcat88@users.noreply.github.com>
2025-09-01 00:58:51 +0800

    Update predict.py

--- predict.py
+++ predict.py
@@ -4,13 +4,12 @@ from ultralytics import YOLO
 
 
 if __name__ == '__main__':
-    model = YOLO('runs/train-pose/exp28/weights/best.pt')
+    model = YOLO('best.pt')
     model.predict(source='datasets/images/val',
                   imgsz=1024,
                   project='runs/predict',
                   name='exp',
                   save=True,
-                  #conf=0.5,
-                  #iou=0.5,
                   save_txt=True
+
                 )
