ec11b527e23fc00e2d9b83c5bf3119d726931159
pigcat <52456159+pigcat88@users.noreply.github.com>
2025-09-01 00:59:29 +0800

    Update val.py

--- val.py
+++ val.py
@@ -4,17 +4,13 @@ from ultralytics import YOLO
 
 
 if __name__ == '__main__':
-    #model = YOLO('runs/train-pose/exp3/weights/best.pt')
-    model = YOLO('runs/train-pose/exp65/weights/best.pt')
+    model = YOLO('best.pt')
     model.val(data='fish_keypoint.yaml',
               split='test',
               imgsz=640,
               batch=16,
-              #crop_fraction = 1.0,
-              #iou=0.7,
-              #conf=0.5,
-              #rect=False,
               save_json=False,
-              project='runs/val',
+              project='runs/test',
               name='exp',
+
               )
